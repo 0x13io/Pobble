4d7104ce8c746c4aece448cd812e886e0c6707ad
Oliver Fisher <o.fisher93@gmail.com>
2015-05-04 00:19:35 +0100

    Setup stage drawing

--- js/game.js
+++ js/game.js
@@ -5,6 +5,16 @@ function paint()
 {
 	var cw = canvas.width; //Make these variables easy to get to.
 	var ch = canvas.height;
+	var hcw = cw/2;
+	var hch = ch/2;
+
+	var aspect_x = 2;
+	var aspect_y = 3;
+
+	var w = ( ch / aspect_y ) * aspect_x;
+ 	var h = ( cw / aspect_x ) * aspect_y;
+	var hw = w/2;
+	var hh = h/2;
 
 	//Clear before drawing.
 	context.clearRect( 0, 0, cw, ch );
@@ -13,6 +23,12 @@ function paint()
 	context.beginPath();
 	context.rect( 0, 0, cw, ch );
 	context.fillStyle = "#000";
+	context.fill();
+
+	//Background
+	context.beginPath();
+	context.rect( hcw-hw, hch-hh, w, h );
+	context.fillStyle = "#FFF";
 	context.fill();
 
 	window.requestAnimationFrame( paint );
