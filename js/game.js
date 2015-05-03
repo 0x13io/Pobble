42d0eded7c205dccd4232c47480673cec25bf799
Oliver Fisher <o.fisher93@gmail.com>
2015-05-04 00:50:39 +0100

    Added aimable cannon

--- js/game.js
+++ js/game.js
@@ -1,6 +1,10 @@
 var canvas;
 var context;
 
+var keys = {};
+
+var cannon_ang = -(Math.PI/2);
+
 function paint()
 {
 	var cw = canvas.width; //Make these variables easy to get to.
@@ -9,7 +13,7 @@ function paint()
 	var hch = ch/2;
 
 	var aspect_x = 2;
-	var aspect_y = 2;
+	var aspect_y = 3;
 
 	var w = ( ch / aspect_y ) * aspect_x;
  	var h = ( cw / aspect_x ) * aspect_y;
@@ -40,7 +44,40 @@ function paint()
 	context.rect( 0, 0, w, h );
 	context.fillStyle = "#FFF";
 	context.fill();
-	
+
+	//Update cannon
+	var turnrate = 0.02;
+	if ( keys[ 37 ] )
+	{
+		cannon_ang -= turnrate;
+	}
+	if ( keys[ 39 ] )
+	{
+		cannon_ang += turnrate;
+	}
+
+	//Gun cannon
+	context.beginPath();
+	context.translate( hw, h );
+	context.rotate( cannon_ang );
+	context.rect( (h/15), -(h/30)/2, (h/10), (h/30) );
+	context.rotate( -cannon_ang );
+	context.translate( -hw, -h );
+	context.fillStyle = "#F00";
+	context.fill();
+
+	//Gun rails
+	context.beginPath();
+	context.rect( 0, h-(h/30), w, (h/30) );
+	context.fillStyle = "#FAA";
+	context.fill();
+
+	//Gun body
+	context.beginPath();
+	context.arc( hw, h, (h/10), 0, Math.PI*2, false );
+	context.fillStyle = "#FAA";
+	context.fill();
+
 	context.translate( -(hcw-hw), -(hch-hh) );
 
 	window.requestAnimationFrame( paint );
@@ -50,6 +87,21 @@ window.onresize = function()
 {
 	canvas.width = canvas.parentElement.clientWidth;
 	canvas.height = canvas.parentElement.clientHeight;
+}
+
+window.onkeydown = function( e )
+{
+	if ( !keys[ e.keyCode ] )
+	{
+		keys[ e.keyCode ] = true;
+	}
+	e.preventDefault();
+}
+
+window.onkeyup = function( e )
+{
+	delete keys[ e.keyCode ];
+	e.preventDefault();
 }
 
 window.onload = function()
