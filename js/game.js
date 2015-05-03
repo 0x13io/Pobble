06c57b7c011b343a46294cccbc7d348a61297a6e
Oliver Fisher <o.fisher93@gmail.com>
2015-05-04 00:12:36 +0100

    Simple drawing

--- js/game.js
+++ js/game.js
@@ -1,4 +1,22 @@
 var canvas;
+var context;
+
+function paint()
+{
+	var cw = canvas.width; //Make these variables easy to get to.
+	var ch = canvas.height;
+
+	//Clear before drawing.
+	context.clearRect( 0, 0, cw, ch );
+
+	//Background
+	context.beginPath();
+	context.rect( 0, 0, cw, ch );
+	context.fillStyle = "#000";
+	context.fill();
+
+	window.requestAnimationFrame( paint );
+}
 
 window.onresize = function()
 {
@@ -9,5 +27,7 @@ window.onresize = function()
 window.onload = function()
 {
 	canvas = document.getElementById( "game_canvas" );
+	context = canvas.getContext( "2d" );
 	window.onresize(); //We call this so we can resize the canvas. Not great practice.
+	paint();
 }
